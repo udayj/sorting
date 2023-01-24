5d902d2f216d275fe787b59c9f407fc042005e10
root <jhunjhunwala.uday@gmail.com>
2023-01-24 13:05:01 +0000

    formatting

--- src/inefficient_sort.rs
+++ src/inefficient_sort.rs
@@ -1,12 +1,11 @@
-pub fn sort<T:Ord> (arr: &mut [T]) {
-
-    let length=arr.len();
+pub fn sort<T: Ord>(arr: &mut [T]) {
+    let length = arr.len();
     loop {
-        let mut sorted=true;
-        for i in 0..(length-1) {
-            if arr[i]>arr[i+1] {
-                arr.swap(i,i+1);
-                sorted=false;
+        let mut sorted = true;
+        for i in 0..(length - 1) {
+            if arr[i] > arr[i + 1] {
+                arr.swap(i, i + 1);
+                sorted = false;
             }
         }
         if sorted == true {
@@ -21,8 +20,8 @@ mod tests {
     use super::sort;
     #[test]
     fn basic_test() {
-        let mut x = [5,4,3,2,1];
+        let mut x = [5, 4, 3, 2, 1];
         sort(&mut x);
-        assert_eq!(x,[1,2,3,4,5]);
+        assert_eq!(x, [1, 2, 3, 4, 5]);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod inefficient_sort;
 pub mod efficient_sort;
+pub mod inefficient_sort;
